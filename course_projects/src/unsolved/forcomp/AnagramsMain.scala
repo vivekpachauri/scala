4c0d0ba01b23cce1abcebbb36101deada405a2e5
Vivek Pachauri <devraj0109@gmail.com>
2014-08-31 12:55:35 -0400

    removed anagram project from the unsolved package

--- course_projects/src/unsolved/forcomp/AnagramsMain.scala
+++
@@ -1,8 +0,0 @@
-package unsolved.forcomp
-
-object AnagramsMain extends App {
-  import Anagrams._;
-
-  //assert(wordOccurrences("abcd") === List(('a', 1), ('b', 1), ('c', 1), ('d', 1)))
-  wordOccurrences("abcd").map(x=>println(x))
-}
